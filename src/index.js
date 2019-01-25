c4a89ddafeff93945cefddebe40ca4642ed72444
zhangsj <zhangsj_crmpd@outlook.com>
2019-01-25 16:24:30 +0800

    导航路由

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './components/app/app'
+import { BrowserRouter } from 'react-router-dom'
+import App from './components/router/app'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render((
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+), document.getElementById('root'))
